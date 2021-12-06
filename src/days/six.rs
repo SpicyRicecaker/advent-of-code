443e967eff7096bb7d3e69a5c902c69a57e919b5
Andy Li <awesomenessisgolden@gmail.com>
2021-12-06 07:23:56 -0800

    begin of 6

--- src/days/six.rs
+++ src/days/six.rs
@@ -1,7 +1,14 @@
-struct Fish {
-    days_left: u32, 
-}
-
 pub fn run(state: crate::State) {
+    let mut pool: Vec<u32> = state
+        .input("input6testtest.txt")
+        .split(',')
+        .map(|s| s.parse::<u32>().unwrap())
+        .collect();
 
+    let out: String = pool
+        .iter()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+    println!("{}", out);
 }
