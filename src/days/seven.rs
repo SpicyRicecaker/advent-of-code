20d56cb829e6bb70e44f05f9c290720e1099859b
Andy Li <awesomenessisgolden@gmail.com>
2021-12-07 16:13:29 -0800

    day 7 pt 1

---
+++ src/days/seven.rs
@@ -0,0 +1,48 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
+pub fn run(state: crate::State) {
+    let mut registry: HashMap<u32, u32> = HashMap::new();
+    let mut max = 0;
+    state
+        .input("input7.txt")
+        .split(',')
+        .map(|s| s.parse::<u32>().unwrap())
+        .for_each(|n| {
+            let count = registry.entry(n).or_insert(0);
+            *count += 1;
+            if n > max {
+                max = n;
+            }
+        });
+    println!("max is {}", max);
+    let mut min_fuel_cost = std::u32::MAX;
+    let mut best_position = 0;
+    // let mut temp = Vec::new();
+    (0..=max).into_iter().for_each(|align_position| {
+        let mut fuel_cost = 0;
+        for (position, crabs) in &registry {
+            let fuel_for_one = match align_position.cmp(position) {
+                Ordering::Less => position - align_position,
+                Ordering::Greater => align_position - position,
+                Ordering::Equal => 0,
+            };
+            // println!(
+            //     "difference between {} (current) and {} (median) is {}",
+            //     number, median, tmp_diff
+            // );
+            fuel_cost += fuel_for_one * crabs;
+        }
+        // temp.push(fuel_cost);
+        if fuel_cost < min_fuel_cost {
+            min_fuel_cost = fuel_cost;
+            best_position = align_position;
+        }
+    });
+    // temp.sort_unstable();
+    // for num in temp.iter().take(3) {
+    //     println!("{}", num);
+    // }
+
+    println!("minimum fuel cost: {} at a median of {}", min_fuel_cost, best_position);
+}
--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     let state = config();
 
     // Brings into scope the run function from that module
-    get_run_func_from!(six_two);
+    get_run_func_from!(seven);
 
     run(state);
 }
