1e64f43aa1259c59283aa45d16acaa768945d7b2
SpicyRicecaker <awesomenessisgolden@gmail.com>
2022-12-06 01:15:35 -0800

    six one

---
+++ twenty-two/src/bin/six.rs
@@ -0,0 +1,28 @@
+use std::ops::ControlFlow;
+fn main() {
+    let chars = std::fs::read_to_string("six.txt")
+        .unwrap()
+        .chars()
+        .map(|c| c as u32 - b'a' as u32)
+        .collect::<Vec<u32>>();
+
+    let res = (0..chars.len()).position(|i| {
+        let mut v = vec![false; 26];
+
+        let mut no_rep = true;
+
+        let _ = (i..i+4).try_for_each(|j| {
+            if v[chars[j] as usize]  {
+                no_rep = false;
+                return ControlFlow::Break(0);
+            } else {
+                v[chars[j] as usize] = true;
+            }
+            ControlFlow::Continue(())
+        });
+
+        no_rep
+    }).unwrap() + 4;
+
+    dbg!(res);
+}
