353c694c3952a7e8202305c2a4e695ea2db79e79
SpicyRicecaker <awesomenessisgolden@gmail.com>
2022-12-17 01:36:24 -0800

    twelve one!!!!!

--- twenty-two/src/bin/twelve.rs
+++ twenty-two/src/bin/twelve.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering::*;
 use std::collections::HashMap;
 
 fn main() {
@@ -36,8 +37,21 @@ fn main() {
         })
         .collect();
 
-    let starting = starting.unwrap();
-    let ending = ending.unwrap();
+    let starting = {
+        let Some([y, x]) = starting else {
+            panic!("starting not found");
+        };
+        y * map[0].len() + x
+    };
+
+    let ending = {
+        let Some([y, x]) = ending else {
+            panic!("ending not found");
+        };
+        // dbg!(y, x);
+        y * map[0].len() + x
+    };
+    // dbg!(starting, ending);
 
     // build the adjacency list from what we've been given
     // we'll assign each node a value based on their row * column + column
@@ -74,4 +88,83 @@ fn main() {
             }
         }
     }
+    // now we can do the bellman ford algorithm
+    let res = bellman_ford(starting, ending, adjacency_list);
+    dbg!(res);
+}
+
+#[derive(Debug, Clone, Copy)]
+struct T {
+    accessed: bool,
+    distance_to: usize,
+    predecessor: Option<usize>,
+}
+
+impl Default for T {
+    fn default() -> Self {
+        Self {
+            accessed: false,
+            distance_to: usize::MAX,
+            predecessor: None,
+        }
+    }
+}
+
+// gets the distance from starting to ending
+fn bellman_ford(
+    starting: usize,
+    ending: usize,
+    graph: HashMap<usize, HashMap<usize, usize>>,
+) -> usize {
+    // create an accessed array
+    // create a predecessory array
+    // create a distance array
+    let mut legend: Vec<_> = (0..graph.len()).map(|_| T::default()).collect();
+    // set our distance to starting to be 0
+    {
+        let s = &mut legend[starting];
+        s.distance_to = 0;
+        s.accessed = true;
+    }
+
+    // loop invariant is that so long as we are in this loop the value to some
+    // distance for some node has been mutated
+    loop {
+        let mut update = false;
+        // for each node
+        for vertex in 0..graph.len() {
+            // if we can't reach this node yet, ignore it
+            if !legend[vertex].accessed {
+                continue;
+            }
+            // for each edge of this node
+            for (&neighbor, edge) in graph.get(&vertex).unwrap().iter() {
+                // if adding our current distance to the edge value results
+                // in the distance being less, update everything
+
+                // do not do <=, otherwise there could be an infinite loop?
+                let new_distance = legend[vertex].distance_to.saturating_add(*edge);
+                if new_distance < legend[neighbor].distance_to {
+                    let n = &mut legend[neighbor];
+                    n.accessed = true;
+                    n.distance_to = new_distance;
+                    n.predecessor = Some(vertex);
+
+                    // also specify that an update has occurred
+                    update = true;
+                }
+            }
+        }
+        if !update {
+            break;
+        }
+    }
+
+    // for i in 0..legend.len() {
+    //     println!("{i}: {}", legend[i].distance_to);
+    // }
+
+    // dbg!(ending);
+
+    legend[ending].distance_to
 }
