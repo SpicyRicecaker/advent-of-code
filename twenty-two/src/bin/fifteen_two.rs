703f161c85f9800534a8be612418def4259f02e9
SpicyRicecaker <awesomenessisgolden@gmail.com>
2022-12-20 06:26:01 -0800

    day 15 done!!!

--- twenty-two/src/bin/fifteen_two.rs
+++ twenty-two/src/bin/fifteen_two.rs
@@ -1,8 +1,8 @@
-use std::ops::ControlFlow;
-use regex::Regex;
 use rayon::prelude::*;
+use regex::{CaptureMatches, Regex};
 use std::sync::mpsc::sync_channel;
 use std::thread;
+use std::time::Instant;
 
 #[derive(Debug, Clone, Copy)]
 struct Sensor {
@@ -10,105 +10,120 @@ struct Sensor {
     y: i32,
     // it cannot be this distance, and because there's no ambiguity it also
     // invalidates the border
-    manhattan_dist: u32,
+    m_d: u32,
 }
 
-const MAX: u32 = 20;
-const ONE_MORE: u32 = MAX + 1;
+const MAX: u32 = 4_000_000;
 
 // we also need a list of beacons, because border
-fn get_invalid_from_row<'a>(
-    row: i32,
-    sensors: &[Sensor],
-    invalid: &'a mut Vec<bool>,
-) -> (&'a mut Vec<bool>, u32) {
-    let mut invalids = 0;
-
-    for sensor in sensors {
-        // add the distance from row to sensor
-        let distance_to_row = row.abs_diff(sensor.y);
-
-        // if the absolute value
-        if distance_to_row > sensor.manhattan_dist {
-            continue;
-        }
-
-        let dist_for_x = (sensor.manhattan_dist - distance_to_row) as i32;
-        // this is the breathing room left for x. We can invalidate all within the ra
-        for x in (sensor.x - dist_for_x).max(0)..=(sensor.x + dist_for_x).min(MAX as i32) {
-            if !invalid[x as usize] {
-                invalid[x as usize] = true;
-                invalids += 1;
-                if invalids > MAX {
-                    return (invalid, invalids);
-                }
+fn get_invalid_from_row(row: i32, sensors: &[Sensor]) -> Option<u32> {
+    let mut intervals: Vec<(i32, i32)> = sensors
+        .iter()
+        .filter_map(|sensor| {
+            // dbg!(sensor);
+            let distance_to_row = row.abs_diff(sensor.y);
+            // dbg!(distance_to_row);
+            if distance_to_row <= sensor.m_d {
+                let distance_left = sensor.m_d - distance_to_row;
+                // dbg!(distance_left);
+                Some((
+                    (sensor.x - distance_left as i32).max(0),
+                    (sensor.x + distance_left as i32).min(MAX as i32),
+                ))
+            } else {
+                None
             }
+        })
+        .collect();
+
+    intervals.sort();
+    // dbg!(&intervals);
+
+    let mut x = vec![];
+
+    let last_interval = intervals
+        .into_iter()
+        .reduce(|(x_1s, x_1e), (x_2s, x_2e)| {
+            if x_2s <= x_1e + 1 {
+               (x_1s, x_2e.max(x_1e))
+            } else {
+                x.push((x_1s, x_1e));
+                (x_2s, x_2e)
+            }
+        })
+        .unwrap();
+
+    x.push(last_interval);
+
+    // dbg!(&x);
+
+    if x.len() == 2 {
+        Some(((x[1].0 - x[0].1) / 2 + x[0].1) as u32)
+    } else {
+        // check if the first or last edge is not included
+        if !(x[0].0..=x[0].1).contains(&0) {
+            Some(0)
+        } else if !(x[0].0..=x[0].1).contains(&(MAX as i32)) {
+            Some(MAX)
+        } else {
+            None
         }
     }
-    (invalid, invalids)
 }
 
 fn main() {
-    let r = Regex::new(r#"x=(-?\d+), y=(-?\d+)"#).unwrap();
+    let r = Regex::new(r#"(-?\d+)"#).unwrap();
+
+    let c = move |m: &mut CaptureMatches| m.next().unwrap()[0].parse::<i32>().unwrap();
 
     let sensors: Vec<_> = std::fs::read_to_string("fifteen.txt")
         .unwrap()
         .lines()
         .map(|l| {
             let mut r = r.captures_iter(l);
+            let r = &mut r;
 
-            let g = r.next().unwrap();
-            let (sx, sy) = (g[1].parse::<i32>().unwrap(), g[2].parse::<i32>().unwrap());
+            let (sx, sy) = (c(r), c(r));
 
-            let g = r.next().unwrap();
-            let (bx, by) = (g[1].parse::<i32>().unwrap(), g[2].parse::<i32>().unwrap());
+            let (bx, by) = (c(r), c(r));
 
             Sensor {
                 x: sx,
                 y: sy,
-                manhattan_dist: sx.abs_diff(bx) + sy.abs_diff(by),
+                m_d: sx.abs_diff(bx) + sy.abs_diff(by),
             }
         })
         .collect();
+    // dbg!(&sensors);
 
-    // dbg!(v);
+    // let (sender, receiver) = sync_channel(0);
 
-    // each row has 4_000_000 possible locations
-    // each column has 4_000_000 possible locations
+    // adding the counter code below slows the code by like 700x lol
 
-    let invalid: Vec<bool> = vec![false; ONE_MORE as usize];
+    // let mut counter = 0;
+    // thread::spawn(move || loop {
+    //     if receiver.try_recv().is_ok() {
+    //         counter += 1;
+    //         println!("processed: {:.2}%", (counter as f64 / MAX as f64) * 100f64);
+    //     }
+    // });
 
-    let (sender, receiver) = sync_channel(0);
+    let instant = Instant::now();
 
-    let mut counter = 0;
-    thread::spawn(move || loop {
-        if receiver.try_recv().is_ok() {
-            counter += 1;
-            // println!("{counter}");
-            println!("processed: {:.2}%", (counter as f64 / MAX as f64) * 100f64);
-        }
-    });
+    let (row, x) = (0..=MAX as i32)
+        .into_par_iter()
+        .find_map_any(move |row| {
+        // .find_map(move |row| {
+            // let t = sender.clone();
+            // t.send(1).unwrap();
+            get_invalid_from_row(row, &sensors).map(|x| (row, x))
+        })
+        .unwrap();
 
-    (0..=MAX as i32).into_par_iter().try_for_each(move |row| {
-        // let instant = Instant::now();
-        let mut invalid = invalid.clone();
-        let (set, l) = get_invalid_from_row(row, &sensors, &mut invalid);
-        // dbg!(l);
-        if l == MAX {
-            let x = set.iter().position(|p| !*p).unwrap();
-            let mut string = String::new();
-            string.push_str(&format!("found beacon slot at x: {x}, y: {row}"));
-            string.push_str(&format!("res: {}", x as u32 * 4000000 + row as u32));
-            std::fs::write("bob.txt", string).unwrap();
-            return ControlFlow::Break(());
-            // break;
-        }
-        let t = sender.clone();
-        t.send(1).unwrap();
-        // dbg!(instant.elapsed());
-        // dbg!("estimated time left:", instant.elapsed() * MAX);
-        ControlFlow::Continue(())
-    });
+    dbg!((row, x));
+    dbg!(x as usize * 4_000_000usize + row as usize);
+
+    dbg!(instant.elapsed());
 
     // let res = get_invalid_from_row(10, &sensors, &beacons);
 }
