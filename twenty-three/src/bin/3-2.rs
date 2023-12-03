623634448954fa43d56b0cd5b1b49f27c75ebbde
SpicyRicecaker <awesomenessisgolden@gmail.com>
2023-12-03 01:43:43 -0800

    dine done

--- twenty-three/src/bin/3-2.rs
+++ twenty-three/src/bin/3-2.rs
@@ -1,4 +1,7 @@
-use std::error::Error;
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // loop through, add all numbers to registry
@@ -9,9 +12,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .map(|l| l.chars().collect::<Vec<char>>())
         .collect();
 
-    // dbg!(&map);
-
-    let mut sum = 0;
+    let mut pivot_registry: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
 
     let mut i = 0;
     let mut j = 0;
@@ -28,41 +29,39 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             // grab num
             let mut num = String::new();
-            let mut connected = false;
+            let mut pivot_connections: HashSet<(usize, usize)> = HashSet::new();
             loop {
                 // print!("{}", map[i][j]);
                 num.push(map[i][j]);
                 // look around
-                if !connected {
-                    'a: for dy in -1..=1 {
-                        for dx in -1..=1 {
-                            // let dbg_top = dy == 1 && dx == 0 && j == 3;
-                            // if dbg_top {
-                            //     println!("hello mom {}", map[i][j]);
-                            // }
+                'a: for dy in -1..=1 {
+                    for dx in -1..=1 {
+                        // let dbg_top = dy == 1 && dx == 0 && j == 3;
+                        // if dbg_top {
+                        //     println!("hello mom {}", map[i][j]);
+                        // }
 
-                            if dy == 0 && dx == 0 {
-                                continue;
-                            }
+                        if dy == 0 && dx == 0 {
+                            continue;
+                        }
 
-                            let newi: i32 = i as i32 + dy;
-                            let newj: i32 = j as i32 + dx;
-                            // if dbg_bot_left {
-                            //     println!("{newi},{newj}");
-                            // }
-                            // ensure dy dx are in range
-                            if !(0..(map.len() as i32)).contains(&newi)
-                                || !(0..(map[0].len() as i32)).contains(&newj)
-                            {
-                                continue;
-                            }
-                            let newi = newi as usize;
-                            let newj = newj as usize;
+                        let newi: i32 = i as i32 + dy;
+                        let newj: i32 = j as i32 + dx;
+                        // if dbg_bot_left {
+                        //     println!("{newi},{newj}");
+                        // }
+                        // ensure dy dx are in range
+                        if !(0..(map.len() as i32)).contains(&newi)
+                            || !(0..(map[0].len() as i32)).contains(&newj)
+                        {
+                            continue;
+                        }
+                        let newi = newi as usize;
+                        let newj = newj as usize;
 
-                            if map[newi][newj] != '.' && !map[newi][newj].is_numeric() {
-                                connected = true;
-                                break 'a;
-                            }
+                        if map[newi][newj] != '.' && !map[newi][newj].is_numeric() {
+                            pivot_connections.insert((newi, newj));
+                            break 'a;
                         }
                     }
                 }
@@ -75,9 +74,11 @@ fn main() -> Result<(), Box<dyn Error>> {
             // println!("ending off on {j}");
             // println!("num is {num}");
             // convert num into actual
-            if connected {
-                sum += num.parse::<u32>().unwrap();
+            for pivot in pivot_connections.into_iter() {
+                let e = pivot_registry.entry(pivot).or_default();
+                e.push(num.parse::<u32>().unwrap());
             }
+
             j += 1;
         }
         // println!();
@@ -85,6 +86,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         i += 1;
     }
 
+    let sum: u32 = pivot_registry
+        .values()
+        .filter(|v| v.len() == 2)
+        .map(|v| v[0] * v[1])
+        .sum();
     println!("{sum}");
 
     Ok(())
